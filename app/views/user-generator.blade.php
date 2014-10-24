678fb4c0b51afe42517782578015ac8c283c20dd
Daniel Little <little@fas.harvard.edu>
2014-10-24 10:38:20 -0700

    update social links/features

--- app/views/user-generator.blade.php
+++ app/views/user-generator.blade.php
@@ -1,40 +1,49 @@
 @extends('_master')
 
 @section('title')
-user-generator
+Political Random User-generator
 @stop
 @section('content')
+
   {{ Form::open(array('url' => 'user-generator', 'method' => 'get')) }}
   {{ Form::label('num_users', 'How many users?') }}
-  {{ Form::number('num_users',Input::get('num_users')) }}
+  {{ Form::number('num_users',Input::get('num_users')) }}<br>
+  {{ Form::label('add_social', 'Add Social links?') }}
+  {{ Form::checkbox('add_social', 'add_social', Input::get('add_social')) }}<br>
   {{ Form::submit('Generate') }}
   {{ Form::close() }}
+
   @if(Input::has('num_users'))
-  <?PHP shuffle ( $legislators['objects'])  ?>
-  @for ($i = 0; $i < Input::get('num_users'); $i++)
-       <?PHP $value = $legislators['objects'][$i];?>
-      <p>
-      <hr>
-      First Name:  {{ $value['person']['firstname'] }}<br>
-      Last Name:  {{ $value['person']['lastname'] }}<br>
-      Birthday:  {{ $value['person']['birthday'] }}<br>
-      Phone:  {{ $value['phone'] }}<br>
-      Gender:  {{ $value['person']['gender_label'] }}<br>
-      Title:  {{ $value['title_long'] }}<br>
-      State:  {{ $value['state'] }}<br>
-      @if ($value['website'])
-      Website:  <A href="{{ $value['website' ]}}" target="_new">click to view</A><br>
-      @endif
-      @if (array_key_exists('twitterid' , $value['person']) and $value['person']['twitterid'])
-      Twitter:  <a href="https://twitter.com/{{$value['person']['twitterid']}}" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @{{ $value['person']['twitterid'] }}</a>
-      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-      {{ $value['person']['twitterid'] }}<br>
-      @endif
-      @if (array_key_exists('youtubeid', $value['person']))
-      <a href="youtube.com/user/{{$value['person']['youtubeid']}}"><img src="https://s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif"/></a>
-      </p>
-        @endif
-  @endfor;
-@endif
+
+      <?PHP shuffle ( $legislators['objects']);?>
+
+      @for ($i = 0; $i < Input::get('num_users'); $i++)
+          <?PHP $value = $legislators['objects'][$i];?>
+          <p><hr>
+          First Name:  {{ $value['person']['firstname'] }}<br>
+          Last Name:  {{ $value['person']['lastname'] }}<br>
+          Birthday:  {{ $value['person']['birthday'] }}<br>
+          Phone:  {{ $value['phone'] }}<br>
+          Gender:  {{ $value['person']['gender_label'] }}<br>
+          Title:  {{ $value['title_long'] }}<br>
+          State:  {{ $value['state'] }}<br>
+
+          @if (Input::get('add_social'))
+              @if (array_key_exists('youtubeid', $value['person']) and $value['person']['youtubeid'])
+                  <a target="_new" href="http://youtube.com/user/{{$value['person']['youtubeid']}}"><img width="32" height="32" src="/images/youtube.png"/></a>
+              @endif
+
+              @if (array_key_exists('twitterid', $value['person']) and $value['person']['twitterid'])
+                  <a href="https://twitter.com/{{$value['person']['twitterid']}}" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @{{ $value['person']['twitterid'] }}</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+              @endif
+
+              @if ($value['website'])
+                  <A href="{{ $value['website' ]}}" target="_new"><img src="/images/www-web.jpeg"/></A>
+              @endif
+          @endif
+
+      @endfor
+  @endif
 @stop
 
--- readme.md
+++ readme.md
@@ -23,3 +23,7 @@ Documentation for the entire framework can be found on the [Laravel website](htt
 ### License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
+
+Outside Contributions:
+Web icon -- www.:qsofticons.com
+Twitter (and related dependendcies) https://about.twitter.com/resources/buttons#follow
